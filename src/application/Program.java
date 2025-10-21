7f8734152ce76240f6b96972a98455ee05ff44cb
Allisson Camargo <allisson.camargopr@gmail.comgit config --global user.email allisson.camargopr@gmail.comgit config --global user.email allisson.camargopr@gmail.com>
2025-10-21 16:30:36 -0300

    Feat: Method Insert - implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,12 @@ public class Program {
             System.out.println();
         }
 
+        System.out.println("=== TESTE 4: sellerInsert ===");
+
+        Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", new Date(), 4000.00, dep);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
 
     }
 }
