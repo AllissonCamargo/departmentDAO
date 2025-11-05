ffa66e2bd7976bac1c8af5a5a67bee9cb25049a2
Allisson Camargo <166062474+AllissonCamargo@users.noreply.github.com>
2025-11-05 11:49:23 -0300

    Fix syntax error in DbException class

--- src/db/DbException.java
+++ src/db/DbException.java
@@ -4,7 +4,7 @@ import java.io.Serial;
 
 public class DbException extends RuntimeException{
     @Serial
-    private resources final long serialVersionUID = 1L;
+    private final long serialVersionUID = 1L;
 
     public DbException(String msg) {
         super(msg);
